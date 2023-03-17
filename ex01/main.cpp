b7c654e012e916cd057a8a769561c0e8d6e223d0
ayoub naji el idrissi <ayoubidrissi00@outlook.com>
2023-03-18 00:55:11 +0100

    day 09

---
+++ ex01/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stack>
+#include <cstdlib>
+
+int main(int ac, char** av) {
+    if (ac != 2)
+        std::cerr << "invalid arguments" << std::endl;
+    char* equation = av[1];
+	char* item;
+	item = strtok(equation, " ");
+	std::cout << item << std::endl;
+	item = strtok(equation, " ");
+	std::cout << item << std::endl;
+	// while ((item = strtok(equation, " "))) {
+		// std::cout << item << std::endl;
+	// }
+	
+}
