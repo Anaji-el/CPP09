f2cbb066ba36781ccc0d79736e89df7b25a028be
ayoub naji el idrissi <ayoubidrissi00@outlook.com>
2023-04-03 02:57:27 +0000

    chore read To Do's

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,11 +6,12 @@
 /*   By: anaji-el <anaji-el@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 13:09:03 by anaji-el          #+#    #+#             */
-/*   Updated: 2023/04/02 07:27:16 by anaji-el         ###   ########.fr       */
+/*   Updated: 2023/04/03 02:56:17 by anaji-el         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
+#include <sstream>
 
 std::string remove_spaces(const std::string &str)
 {
@@ -35,8 +36,11 @@ bool is_valid(const std::string &str)
 		}
 		else
 		{
-			if (!isdigit(str[i]) && str[i] != '.' && str[i] != '-')
+			if(str[i] == '.')
+				continue;
+			if (!(isdigit(str[i]) && str[i] != '-' ) || (str.find(".") != str.rfind(".")))
 				return false;
+			// if (!(isdigit(str[i]) && str[i] != '.' && str[i] != '-') || (str.find(".") != str.rfind(".")))
 		}
 	}
 	return true;
@@ -62,9 +66,9 @@ bool frfr(int year, int month, int day)
 	return (!(year <= y) || !(month <= m) || !(day < d));
 }
 
-std::multimap<std::string, double> read_data()
+std::map<std::string, double> read_data()
 {
-	std::multimap<std::string, double> data;
+	std::map<std::string, double> data;
 	std::string filename = "data.csv";
 	std::ifstream file(filename);
 
@@ -77,10 +81,32 @@ std::multimap<std::string, double> read_data()
 		std::string key(p);
 		p = std::strtok(NULL, ",");
 		double value = std::atof(p);
-		data.insert(std::pair<std::string,double>(key,value));
+		data.insert(std::pair<std::string, double>(key, value));
 	}
 	return (data);
 }
+
+// std::map<std::string, double> read_data()
+// {
+// 	std::map<std::string, double> data;
+// 	std::string filename = "data.csv";
+// 	std::ifstream bla1(filename);
+
+// 	std::string arr;
+// 	std::string arr1, b;
+
+// 	if (!bla1)
+// 		std::cerr << "cannot open this file!" << filename << std::endl;
+
+// 	while (std::getline(bla1, b))
+// 	{
+// 		std::stringstream bla(b);
+// 		std::getline(bla, arr, ',');
+// 		std::getline(bla, arr1, ',');
+// 		data[arr] = std::atof(arr1.c_str());
+// 	}
+// 	return (data);
+// }
 bool check_date(int year, int month, int day)
 {
 
@@ -107,28 +133,28 @@ void get_date(std::string line)
 		std::cerr << "invalid arg" << std::endl;
 	else
 	{
+		
 		int year = std::atof((trimmed.substr(0, 4).c_str()));
 		int month = std::atof((trimmed.substr(5, 2).c_str()));
 		int day = std::atof((trimmed.substr(8, 2).c_str()));
-		//int value = std::atof((trimmed.substr(11, 4).c_str()));
 		int len = trimmed.length() + trimmed.find('|') + 1;
 		double value = std::atof((trimmed.substr(trimmed.find('|') + 1, len).c_str()));
 		std::string date = trimmed.substr(0, trimmed.find('|'));
-		if (!check_date(year, month, day) || !(value >= 0 && value <= 1000) ||  value < 0)
+		if (!check_date(year, month, day) || !(value >= 0 && value <= 1000) || value < 0)
 		{
 			if (!check_date(year, month, day))
-				std::cerr << "Error: bad input => " << date <<  std::endl;
+				std::cerr << "Error: bad input => " << date << std::endl;
 			else if (value < 0)
-				std::cerr << "Error : not a positive number."<< std::endl;
+				std::cerr << "Error : not a positive number." << std::endl;
 			else if (value > 1000)
 				std::cerr << "Error: too large a number." << std::endl;
 		}
 		else
 		{
-			std::multimap<std::string, double> data = read_data();
-			std::multimap<std::string, double>::iterator it;
+			std::map<std::string, double> data = read_data();
+			std::map<std::string, double>::iterator it;
 			it = data.find(date);
-			if(it == data.end())
+			if (it == data.end())
 			{
 				it = data.lower_bound(date);
 				it--;
@@ -155,15 +181,26 @@ int main(int ac, char **av)
 		return 1;
 	}
 	std::string line;
-
+	
+	if(std::getline(file, line))
+	{
+		if (strcmp("date | value", line.c_str()))
+		{
+			std::cout << "Error: cannot find data or value" << std::endl;
+			exit(1);
+		}
+	}
 	while (std::getline(file, line))
 	{
-
-		get_date(line);
-		is_valid(line);
-	}
+		if (line.empty() || isspace(line))
+		{
+			continue; // skip empty or whitespace-only line
+		}
+			get_date(line);
+			is_valid(line);
+		}
 	return 0;
 }
-//to do's : abort when i add new lines in input 
-//need to skip first line in input
-//need to check name of file 
+// to do's : abort when i add new lines in input and whitespaces
+// need to skip first line in input
+// need to check name of file
