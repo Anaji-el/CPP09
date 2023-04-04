abfb7c486ab419b54e09d871aa75baa2781f8954
ayoub naji el idrissi <ayoubidrissi00@outlook.com>
2023-04-04 07:13:48 +0000

    chore : EX00,EX01 DONE

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: anaji-el <anaji-el@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 13:09:03 by anaji-el          #+#    #+#             */
-/*   Updated: 2023/04/03 02:56:17 by anaji-el         ###   ########.fr       */
+/*   Updated: 2023/04/04 06:40:36 by anaji-el         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,9 +36,9 @@ bool is_valid(const std::string &str)
 		}
 		else
 		{
-			if(str[i] == '.')
+			if (str[i] == '.')
 				continue;
-			if (!(isdigit(str[i]) && str[i] != '-' ) || (str.find(".") != str.rfind(".")))
+			if ((!isdigit(str[i]) && str[i] != '-') || (str.find(".") != str.rfind(".")))
 				return false;
 			// if (!(isdigit(str[i]) && str[i] != '.' && str[i] != '-') || (str.find(".") != str.rfind(".")))
 		}
@@ -46,7 +46,7 @@ bool is_valid(const std::string &str)
 	return true;
 }
 
-bool frfr(int year, int month, int day)
+bool data_(int year, int month, int day)
 {
 	std::string filename = "data.csv";
 	std::ifstream file(filename);
@@ -63,7 +63,14 @@ bool frfr(int year, int month, int day)
 	int y = std::atof((line.substr(0, 4).c_str()));
 	int m = std::atof((line.substr(5, 2).c_str()));
 	int d = std::atof((line.substr(8, 2).c_str()));
-	return (!(year <= y) || !(month <= m) || !(day < d));
+	if (year < y)
+		return false;
+	else if (year == y && month < m)
+		return false;
+	else if (year == y && month == m && day < d)
+		return false;
+	else
+		return true;
 }
 
 std::map<std::string, double> read_data()
@@ -86,33 +93,9 @@ std::map<std::string, double> read_data()
 	return (data);
 }
 
-// std::map<std::string, double> read_data()
-// {
-// 	std::map<std::string, double> data;
-// 	std::string filename = "data.csv";
-// 	std::ifstream bla1(filename);
-
-// 	std::string arr;
-// 	std::string arr1, b;
-
-// 	if (!bla1)
-// 		std::cerr << "cannot open this file!" << filename << std::endl;
-
-// 	while (std::getline(bla1, b))
-// 	{
-// 		std::stringstream bla(b);
-// 		std::getline(bla, arr, ',');
-// 		std::getline(bla, arr1, ',');
-// 		data[arr] = std::atof(arr1.c_str());
-// 	}
-// 	return (data);
-// }
 bool check_date(int year, int month, int day)
 {
-
-	if (frfr(year, month, day))
-	{
-		if (year < 2009 || month <= 0 || month > 12 || day < 1 || day > 31)
+		if (year < 1000 || month <= 0 || month > 12 || day < 1 || day > 31)
 			return false;
 		if (month == 2 && (day > 28))
 			return false;
@@ -121,54 +104,54 @@ bool check_date(int year, int month, int day)
 		if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30)
 			return false;
 		return true;
-	}
-	return false;
 }
 
-void get_date(std::string line)
+void get_date(std::string line, std::map<std::string, double> &data)
 {
 	std::string trimmed = remove_spaces(line);
 	// std::cout << trimmed << std::endl;
+	std::map<std::string, double>::iterator it;
 	if (!is_valid(trimmed))
-		std::cerr << "invalid arg" << std::endl;
+		std::cerr << "Error" << std::endl;
 	else
 	{
-		
+		double value = 0;
 		int year = std::atof((trimmed.substr(0, 4).c_str()));
 		int month = std::atof((trimmed.substr(5, 2).c_str()));
 		int day = std::atof((trimmed.substr(8, 2).c_str()));
 		int len = trimmed.length() + trimmed.find('|') + 1;
-		double value = std::atof((trimmed.substr(trimmed.find('|') + 1, len).c_str()));
+		if (trimmed.find('|') != ULONG_MAX)
+			value = std::atof((trimmed.substr(trimmed.find('|') + 1, len).c_str()));
 		std::string date = trimmed.substr(0, trimmed.find('|'));
 		if (!check_date(year, month, day) || !(value >= 0 && value <= 1000) || value < 0)
 		{
 			if (!check_date(year, month, day))
 				std::cerr << "Error: bad input => " << date << std::endl;
-			else if (value < 0)
+			if (value < 0)
 				std::cerr << "Error : not a positive number." << std::endl;
 			else if (value > 1000)
 				std::cerr << "Error: too large a number." << std::endl;
 		}
+		else if (!data_(year, month, day))
+			std::cout << "2009-01-02 => " << value << " = " << 0 * value << std::endl;
 		else
 		{
-			std::map<std::string, double> data = read_data();
-			std::map<std::string, double>::iterator it;
 			it = data.find(date);
 			if (it == data.end())
 			{
 				it = data.lower_bound(date);
 				it--;
 			}
-			std::cout << date << " => " << value << " = " << it->second * value << std::endl;
+			// std::cout << it->first << " => " << value << " = " << it->second * value << std::endl;
 			// std::cout << trimmed.substr(0, trimmed.find('|')) << "\n"<< "The lowest bound is: " << it->second << std::endl;
-			// std::cout << year << "-" << month << "-" << day << " | " << value << "" << std::endl;
+			std::cout << date << " => " << value << " = " << it->second * value << std::endl;
 		}
 	}
 }
 
 int main(int ac, char **av)
 {
-	if (ac != 2)
+	if (ac  != 2)
 	{
 		std::cerr << "invalid arguments" << std::endl;
 		exit(1);
@@ -180,25 +163,26 @@ int main(int ac, char **av)
 		std::cerr << "cannot open this file!" << filename << std::endl;
 		return 1;
 	}
-	std::string line;
-	
-	if(std::getline(file, line))
+	std::string  line;
+
+	if (std::getline(file, line))
 	{
-		if (strcmp("date | value", line.c_str()))
+		if (line.find("date") == ULONG_MAX || line.find("|") == ULONG_MAX || line.find("value") == ULONG_MAX)
 		{
 			std::cout << "Error: cannot find data or value" << std::endl;
 			exit(1);
 		}
-	}
-	while (std::getline(file, line))
-	{
-		if (line.empty() || isspace(line))
+		std::map<std::string, double> data = read_data();
+		while (std::getline(file, line))
 		{
-			continue; // skip empty or whitespace-only line
-		}
-			get_date(line);
+			if (line.empty() || remove_spaces(line).empty())
+			{
+				continue;
+			}
+			get_date(line, data);
 			is_valid(line);
 		}
+	}
 	return 0;
 }
 // to do's : abort when i add new lines in input and whitespaces
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,17 +2,61 @@
 #include <stack>
 #include <cstdlib>
 
-int main(int ac, char** av) {
-    if (ac != 2)
-        std::cerr << "invalid arguments" << std::endl;
-    char* equation = av[1];
-	char* item;
-	item = strtok(equation, " ");
-	std::cout << item << std::endl;
-	item = strtok(equation, " ");
-	std::cout << item << std::endl;
-	while ((item = strtok(equation, " "))) {
-		std::cout << item << std::endl;
-	}
+std::string removeSpaces(std::string str)
+{
+	str.erase(std::remove(str.begin(), str.end(), ' '), str.end());
+	return str;
+}
 
+int main(int ac, char **av)
+{
+	if (ac != 2)
+		std::cerr << "invalid arguments" << std::endl;
+	std::string trimmed = removeSpaces(av[1]);
+	std::stack<int> stack;
+	for (size_t i = 0; i < trimmed.length(); i++)
+	{
+		char c = trimmed[i];
+		if (isdigit(c))
+			stack.push(static_cast<int>(c) - 48);
+		else if (c == '+' || c == '-' || c == '*' || c == '/')
+		{
+			if (stack.size() < 2)
+			{
+				std::cerr << "Not enough numbers" << std::endl;
+				exit(1);
+			}
+			int first = stack.top();
+			stack.pop();
+			int second = stack.top();
+			stack.pop();
+			switch (c)
+			{
+			case '*':
+				stack.push(second * first);
+				break;
+			case '/':
+				stack.push(second / first);
+				break;
+			case '+':
+				stack.push(second + first);
+				break;
+			case '-':
+				stack.push(second - first);
+				break;
+			}
+		}
+		else
+		{
+			std::cerr << "Wrong character" << std::endl;
+			exit(1);
+		}
+	}
+	if (stack.size() != 1)
+	{
+		std::cerr << "Stack not empty" << std::endl;
+		exit(1);
+	}
+	std::cout << stack.top() << std::endl;
+	return (0);
 }
