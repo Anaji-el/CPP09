aec4fb42d4be13afe13504b4b0b3094869001614
ayoub naji el idrissi <ayoubidrissi00@outlook.com>
2023-04-07 02:49:45 +0000

    ::

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -1,23 +1,81 @@
 #include "PmergeMe.hpp"
 
-PmergeMe::PmergeMe()
+std::pair<int, std::string> make_int_string_pair(int x, const std::string &s)
 {
+	return std::make_pair(x, s);
 }
 
-/*--------------------------------------------------------*/
-PmergeMe::PmergeMe(const PmergeMe &a)
+std::vector<int> _vector(int ac, char *av[], std::vector<std::pair<unsigned int, unsigned int>  > &container,
+					std::vector<unsigned int> &cont, std::vector<unsigned int> &conta)
 {
-    this->operator=(a);
+	clock_t start_time, end;
+	double diff;
+	unsigned int tmp;
+
+	start_time = clock();
+	for (size_t i = 0; i < container.size(); i++)
+	{
+		if (container[i].first > container[i].second)
+			std::swap(container[i].first, container[i].second);
+	}
+
+	for (size_t i = 0; i < container.size(); i++)
+		conta.push_back(container[i].first);
+
+	for (size_t i = 0; i < container.size(); i++)
+		cont.push_back(container[i].second);
+
+	std::sort(conta.begin(), conta.end());
+
+	for (size_t i = 0; i < cont.size(); i++)
+		conta.insert(std::lower_bound(begin(conta), conta.end(), cont[i]), cont[i]);
+	if (false)
+		conta.insert(std::lower_bound(conta.begin(), conta.end(), tmp), tmp);
+	std::cout << "\nAfter : ";
+
+	for (size_t i = 0; i < conta.size(); i++)
+		std::cout << conta[i] << " ";
+	std::cout << std::endl;
+
+	end = clock();
+	diff = double(end - start_time) / 1000;
+	std::cout << "Time to process a range of " << ac << " elements with v : " << diff << " us"
+			  << std::endl;
 }
 
-/*--------------------------------------------------------*/
-PmergeMe::~PmergeMe()
+std::deque<int> _deque(int ac, char *av[], std::deque<std::pair<unsigned int, unsigned int> > &container,
+					std::deque<unsigned int> &cont, std::deque<unsigned int> &conta)
 {
-}
+	clock_t start_time, end;
+	double diff;
+	unsigned int tmp;
 
-/*--------------------------------------------------------*/
-PmergeMe &PmergeMe::operator=(const PmergeMe &a)
-{
-    (void)a;
-    return (*this);
+	start_time = clock();
+	for (size_t i = 0; i < container.size(); i++)
+	{
+		if (container[i].first > container[i].second)
+			std::swap(container[i].first, container[i].second);
+	}
+
+	for (size_t i = 0; i < container.size(); i++)
+		conta.push_back(container[i].first);
+
+	for (size_t i = 0; i < container.size(); i++)
+		cont.push_back(container[i].second);
+
+	std::sort(conta.begin(), conta.end());
+
+	for (size_t i = 0; i < cont.size(); i++)
+		conta.insert(std::lower_bound(begin(conta), conta.end(), cont[i]), cont[i]);
+	if (false)
+		conta.insert(std::lower_bound(conta.begin(), conta.end(), tmp), tmp);
+	// std::cout << "\nAfter : ";
+
+	// for (size_t i = 0; i < conta.size(); i++)
+	//     std::cout << conta[i] << " ";
+
+	end = clock();
+	diff = double(end - start_time) / 1000;
+	std::cout << "\nTime to process a range of " << ac << " elements with d : " << diff << " us"
+			  << std::endl;
 }
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -9,15 +9,10 @@
 #include <sys/time.h>
 #include <unistd.h>
 
-class PmergeMe
-{
-private:
-    // your private members here
-public:
-    PmergeMe();
-    PmergeMe(const PmergeMe &a);
-    ~PmergeMe();
-    PmergeMe &operator=(const PmergeMe &a);
-};
+std::vector<int> _vector(int ac, char *av[], std::vector<std::pair<unsigned int, unsigned int> > &container,
+                         std::vector<unsigned int> &cont, std::vector<unsigned int> &conta);
+std::deque<int> _deque(int ac, char *av[], std::deque<std::pair<unsigned int, unsigned int> > &container,
+                       std::deque<unsigned int> &cont, std::deque<unsigned int> &conta);
+std::pair<int, int> make_int_string_pair(int x, int s);
 
 #endif
